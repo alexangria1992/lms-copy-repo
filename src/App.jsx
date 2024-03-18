b78a4b77c4f2b971b49eacbdcb4dbec631950d49
Alexandre Angrignon <alex040892@gmail.com>
2024-03-18 16:55:27 +1300

    new branch test

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import Hero from "./components/Hero/Hero";
 import Navbar from "./components/Navbar/Navbar";
+import OfferSection from "./components/OfferSection/OfferSection";
 
 function App() {
   return (
     <>
       <Navbar />
       <Hero />
+      <OfferSection />
     </>
   );
 }
